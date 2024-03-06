0dfa9b6aaf62eca02b46d164a86c175f5b8a308d
Saleh Alrougi <117602290+IDE21@users.noreply.github.com>
2024-03-06 23:53:25 +0300

    Add files via upload

---
+++ process_data.py
@@ -0,0 +1,27 @@
+#!/usr/bin python3
+
+# Note that the instructions below are only for guidance.
+# If you think of another method then please feel free to do however works for you.
+# Be sure to read the Riyadh_weather.md for additional guidance.  Especially regarding character encoding.
+
+# import packages
+
+
+# File and directory paths
+
+
+# Create the output directory if it doesn't exist
+
+# Load new data
+
+
+# Output path for the combined data
+
+
+# Check if the output file already exists
+
+
+# Append new data to the existing data list
+
+
+# Save the updated data to a JSON file
